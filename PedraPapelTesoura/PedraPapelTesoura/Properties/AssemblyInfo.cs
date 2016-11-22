204d91197cd1266edbf60265b5682bedb3fa8eae
TIAGOCV-PC\tcver <tcverdade.64@gmail.com>
2016-11-22 22:13:41 +0000

    Corrigir o erro do nome!

--- PedraPapelTesoura/PedraPapelTesoura/Properties/AssemblyInfo.cs
+++ PedraPapelTesoura/PedraPapelTesoura/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("PedraPapelTesoura")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("PedraPapelTesoura")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9025e3ad-808a-4bdd-8ebf-050f007d11f2")]
+[assembly: Guid("90125707-a3f4-4c1e-a3eb-cc7b67d07fb7")]
 
 // Version information for an assembly consists of the following four values:
 //
