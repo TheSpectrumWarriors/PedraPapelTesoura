605853cbc1036da09a518f04b66f0f86d7f70f80
RodrigoRosmaninho <quico.rosmaninho@gmail.com>
2016-11-22 22:17:12 +0000

    Adicionar comentários com passos a seguir

--- PedraPapelTesoura/PedraPapelTesoura/Program.cs
+++ PedraPapelTesoura/PedraPapelTesoura/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+4 Fases Principais:
+
+1. Perguntar ao utilizador qual das 3 opções ele escolhe
+2. Verificar que o utilizador respondeu com uma das três opções
+3. Escolher uma das três opções ao acaso
+4. Dizer ao utilizador qual é que o PC escolheu e, de acordo com as reras do jogo, verificar quem ganhou, ou se houve empate. 
+*/
+
 namespace PedraPapelTesoura {
     class Program {
         static void Main(string[] args) {
